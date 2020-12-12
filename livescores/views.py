37218d38aa5ec76b2888330126403883bc289112
Daramola Oluwafemi <dfmopeolwa@gmail.com>
2020-12-12 12:15:11 +0100

    Added all the scraper logic

--- livescores/views.py
+++ livescores/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
-
+from bs4 import BeautifulSoup as BS
+from  urllib.request import Request, urlopen
 # Create your views here.
+
+link='https://livescores.com'
 
 def home(request):
     return render(request, 'base.html')
 
 def scores(request):
 
-    return render(request, 'livescores/scores.html')
+    link_req=Request(link, headers={'user-agent':'XYZ-3.0'})
+    link_open=urlopen(link_req, timeout=20).read()
+    soup= BS(link_open, 'html.parser')
+
+    container=soup.findAll('div',{'class': 'row-gray'})
+
+
+    total=[]
+    for item in container:
+        home_team = item.find('div', {'class': 'ply tright name'}).text
+        away_team = item.find('div', {'class': 'ply name'}).text
+        team_score= item.find('div', {'class': 'sco'}).text
+        match_time= item.find('div', {'class': 'min'}).text
+
+
+        total.append((home_team, away_team, team_score, match_time))
+
+    context={
+        'total':total
+    }
+    return render(request, 'livescores/scores.html', context)
