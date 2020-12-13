835d30540b4735796a0104fbed1dcac7d3847dab
Daramola Oluwafemi <dfmopeolwa@gmail.com>
2020-12-13 22:04:00 +0100

    Heroku commit

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn gunicorn football.wsgi
+release: python manage.py migrate
--- football/settings.py
+++ football/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import  os
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,6 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-import django_heroku
+
 
 django_heroku.settings(locals())
